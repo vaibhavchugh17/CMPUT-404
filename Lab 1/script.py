fbebbfcf9fa2b518785cb53ae70a3d708a1c5e90
Vaibhav <vchugh@ualberta.ca>
2023-01-13 15:53:32 -0700

    Final change

--- Lab 1/script.py
+++ Lab 1/script.py
@@ -1,3 +1,7 @@
 import requests
-print('requests library version: ', requests.__version__)
+
+print('Requests library version: ', requests.__version__)
+print()
 print(requests.get('http://www.google.com/').text)
+print()
+print(requests.get('https://raw.githubusercontent.com/vaibhavchugh17/CMPUT-404/main/Lab%201/script.py').text)
